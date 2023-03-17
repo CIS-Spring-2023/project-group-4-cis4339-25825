f972f9e8a31e6ce4aeec7b3823f1b560372b5f8d
Udayraa <udayrashid102@gmail.com>
2023-03-17 00:15:50 -0500

    services folder

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,9 +3,9 @@ import router from './router'
 import App from './App.vue'
 import './index.css'
 import './service.css'
-//import store from './store';
+import store from './store';
 
 const app = createApp(App)
 app.use(router)
-//app.use(store);
+app.use(store);
 app.mount('#app')
